b460a3933c21c817d4c7a1d8a37f07438118fa1c
AqilMohammad <146756431+AqilMohammad@users.noreply.github.com>
2023-10-02 23:01:25 +0530

    Uploaded

---
+++ RidivTechnology/assignment/invoices/urls.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import InvoiceViewSet, InvoiceDetailViewSet
+
+router = DefaultRouter()
+router.register(r'invoices', InvoiceViewSet)
+router.register(r'invoice-details', InvoiceDetailViewSet)
+
+urlpatterns = [
+    path('', include(router.urls)),
+    path('admin/', admin.site.urls),
+    path('invoices/', include('invoices.urls')),
+]
